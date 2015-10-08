415b870b5e7adc57481431223e7a31038b7fa9ad
stevenzhenhaowang <zwang.steven@mail.com>
2015-10-08 16:22:24 +1100

    commit 3

---
+++ Edis.Db/NoteLinks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edis.Db
+{
+    public class NoteLinks
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        public string NoteId1 { get; set; }
+        public string NoteId2 { get; set; }
+    }
+}
